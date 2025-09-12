5e8f99aad44131e9015a000520f73b3e895fc24e
shaobeichen <shaobeichen@outlook.com>
2025-09-12 14:13:26 +0800

    feat: update github trending

--- save.js
+++ save.js
@@ -13,11 +13,13 @@ const saveToFile = async json => {
 
   // 单个json
   const filename = `./archive/${dirName}.json`
-  await fs.writeFileSync(filename, JSON.stringify(json))
-  const cdnBase = 'https://cdn.jsdelivr.net/gh/shaobeichen/github-trending@main/archive'
-  const cdnUrl = `${cdnBase}/${dirName}.json`
+  if (!fs.readdirSync('./archive').includes(`${dirName}.json`)) {
+    await fs.writeFileSync(filename, JSON.stringify(json))
+  }
 
   // 归总json
+  const cdnBase = 'https://cdn.jsdelivr.net/gh/shaobeichen/github-trending@main/archive'
+  const cdnUrl = `${cdnBase}/${dirName}.json`
   if (!path.includes('total.json')) await fs.writeFileSync('./total.json', JSON.stringify({ urls: [] }))
   const data = await fs.readFileSync('./total.json', 'utf-8')
   const parseData = JSON.parse(data)
